4a5f0f80b4b4313a6c7401758e75736823be5f8e
georgetommy <g.tranchet@gmail.com>
2016-03-05 09:23:50 +0100

    beginning of chronos implementation, needs to correct / reset the time counted

--- MagicTimer/app/src/main/java/com/gatmobile/magictimer/TimerActivity.java
+++ MagicTimer/app/src/main/java/com/gatmobile/magictimer/TimerActivity.java
@@ -6,10 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimerActivity extends AppCompatActivity {
 
     Chronometer chronometer;
     Button start, pause, reset, next;
+
+    List<Chronometer> playersChronos = new ArrayList<Chronometer>();
+    List<Chronometer> playersTotalChronos = new ArrayList<Chronometer>();
+
+    int totalNbPlayers = 6;
+    int currentNbPlayer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,20 @@ public class TimerActivity extends AppCompatActivity {
         pause = (Button) findViewById(R.id.pause_chrono_main);
         reset = (Button) findViewById(R.id.reset_chrono_main);
         next = (Button) findViewById(R.id.next_chrono_main);
+
+        playersChronos.add((Chronometer)findViewById(R.id.chrono_1));
+        playersChronos.add((Chronometer)findViewById(R.id.chrono_2));
+        playersChronos.add((Chronometer)findViewById(R.id.chrono_3));
+        playersChronos.add((Chronometer)findViewById(R.id.chrono_4));
+        playersChronos.add((Chronometer)findViewById(R.id.chrono_5));
+        playersChronos.add((Chronometer)findViewById(R.id.chrono_6));
+
+        playersTotalChronos.add((Chronometer)findViewById(R.id.chrono_total_1));
+        playersTotalChronos.add((Chronometer)findViewById(R.id.chrono_total_2));
+        playersTotalChronos.add((Chronometer)findViewById(R.id.chrono_total_3));
+        playersTotalChronos.add((Chronometer)findViewById(R.id.chrono_total_4));
+        playersTotalChronos.add((Chronometer)findViewById(R.id.chrono_total_5));
+        playersTotalChronos.add((Chronometer)findViewById(R.id.chrono_total_6));
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +69,11 @@ public class TimerActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("saving time for player: " + chronometer.getFormat());
+                System.out.println("saving time for player: " + currentNbPlayer + ", " + chronometer.getFormat());
+                playersChronos.get(currentNbPlayer).stop();
+                currentNbPlayer = ( currentNbPlayer + 1 ) % totalNbPlayers;
+                System.out.println("next player: " + currentNbPlayer + ".");
+                playersChronos.get(currentNbPlayer).start();
             }
         });
 
