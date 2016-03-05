76da3e670d78b992c2bd9e7183d5cce01cc938a2
georgetommy <g.tranchet@gmail.com>
2016-03-05 09:05:56 +0100

    creates the 6 chrono for each player

--- MagicTimer/app/src/main/java/com/gatmobile/magictimer/TimerActivity.java
+++ MagicTimer/app/src/main/java/com/gatmobile/magictimer/TimerActivity.java
@@ -1,30 +1,59 @@
 package com.gatmobile.magictimer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class TimerActivity extends AppCompatActivity {
+
+    Chronometer chronometer;
+    Button start, pause, reset, next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       chronometer = (Chronometer) findViewById(R.id.chronometer_main);
+        start = (Button) findViewById(R.id.start_chrono_main);
+        pause = (Button) findViewById(R.id.pause_chrono_main);
+        reset = (Button) findViewById(R.id.reset_chrono_main);
+        next = (Button) findViewById(R.id.next_chrono_main);
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                chronometer.start();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setFormat(null);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("saving time for player: " + chronometer.getFormat());
+            }
+        });
+
+
+
     }
+
+
 
 }
